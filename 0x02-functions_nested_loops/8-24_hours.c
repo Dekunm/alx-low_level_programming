c152e871b6d24b0984a09d6addda6460f2cc8efe
Dekunm <kunmzxlee@gmail.com>
2023-02-22 08:28:52 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,44 @@
 #include "main.h"
 
 /**
- * jack_bauer - printts every minute of the day of jack bauer
- * sttarting from 00:00 to 23:59
- */
+* jack_bauer - function to print every minute of the day
+*
+* Return: int
+*/
+
 
 void jack_bauer(void)
 {
-	int i , j;
+	int i, j, k, l;
 
-	i =  0;
-
-	while (i < 24)
+	for (i = 0; i < 3; i++)
 	{
-		j = 0;
-		while (j < 60)
+		if (i == 2)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
+			for (j = 0; j < 4; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						printf("%d%d:%d%d\n", i, j, k, l);
+					}
+				}
+			}
 		}
-
-	}	i++;
+		else
+		{
+			for (j = 0; j < 9; j++)
+			for (j = 0; j < 10; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						printf("%d%d:%d%d\n", i, j, k, l);
+					}
+				}
+			}
+		}
+	}
 }
