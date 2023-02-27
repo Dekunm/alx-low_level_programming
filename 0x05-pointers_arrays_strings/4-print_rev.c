b7e40d3a4221719e759ab2d6e241870e0538529e
Dekunm <kunmzxlee@gmail.com>
2023-02-27 12:52:58 -0800

    reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include  "main.h"
+
+/**
+ * print_rev - a reversed string followedby a new  liine
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char*s)
+{
+	int lng = 0;
+
+	int o;
+
+	while (*s != '\0')
+	
+	{
+		lng++;
+		s++;
+	}
+
+	s--;
+
+	for (o = lng; o > 0;  o--)
+	{
+		putchar(*s);
+		s--;
+	}
+
+	putchar('\n');
+}
