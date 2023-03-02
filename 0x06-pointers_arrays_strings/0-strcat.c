ce20798299d0ab505367743b5b59929001bf057f
Dekunm <kunmzxlee@gmail.com>
2023-03-02 09:12:37 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * This concatenates 2 strings
+ * Returns:dest
+ */
+
+char *_strcat(char *dest, char  *src);
+{
+	string1 = "dest";
+	string2 = "src";
+
+	strcat(string1, string2);
+
+	return (string1);
+}
