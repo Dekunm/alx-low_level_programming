6bcc42dfdeb6cba2ceeb529067e4746b668822c7
Dekunm <kunmzxlee@gmail.com>
2023-03-13 15:48:28 -0700

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,38 +10,45 @@ void create_word(char **, char *, int, int, int);
 *
 * Return: returns a pointer to an array of strings (words)
 */
+
 char **strtow(char *str)
 {
-  int i, flag, len;
-  char **words;
+	int i, flag, len;
+	char **words;
 
-  if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
-     return (NULL);
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+	{
+		return (NULL);
+	}
+	i = flag = len = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			flag = 1;
 
-  i = flag = len = 0;
-  while (str[i])
-  {
-     if (flag == 0 && str[i] != ' ')
-        flag = 1;
-     if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-     {
-        flag = 0;
-        len++;
-     }
-     i++;
-  }
+		}
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			flag = 0;
+			len++;
+		}
+		i++;
+	}
 
-  len += flag == 1 ? 1 : 0;
-  if (len == 0)
-     return (NULL);
-
-  words = (char **)malloc(sizeof(char *) * (len + 1));
-  if (words == NULL)
-     return (NULL);
-
-  util(words, str);
-  words[len] = NULL;
-  return (words);
+	len += flag == 1 ? 1 : 0;
+	if (len == 0)
+	{
+		return (NULL);
+	}
+	words = (char **)malloc(sizeof(char *) * (len + 1));
+	if (words == NULL)
+	{
+		return (NULL);
+	}
+	util(words, str);
+	words[len] = NULL;
+	return (words);
 }
 
 /**
@@ -51,29 +58,28 @@ char **strtow(char *str)
 */
 void util(char **words, char *str)
 {
-  int i, j, start, flag;
+	int i, j, start, flag;
 
-  i = j = flag = 0;
-  while (str[i])
-  {
-     if (flag == 0 && str[i] != ' ')
-     {
-        start = i;
-        flag = 1;
-     }
-
-     if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-     {
-        create_word(words, str, start, i, j);
-        j++;
-        flag = 0;
-     }
-
-     i++;
-  }
-
-  if (flag == 1)
-     create_word(words, str, start, i, j);
+	i = j = flag = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+			flag = 1;
+		}
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			create_word(words, str, start, i, j);
+			j++;
+			flag = 0;
+		}
+		i++;
+	}
+	if (flag == 1)
+	{
+		create_word(words, str, start, i, j);
+	}
 }
 
 /**
@@ -84,14 +90,16 @@ void util(char **words, char *str)
 * @end: the stopping index of the word
 * @index: the index of the array to insert the word
 */
+
 void create_word(char **words, char *str, int start, int end, int index)
 {
-  int i, j;
+	int i, j;
 
-  i = end - start;
-  words[index] = (char *)malloc(sizeof(char) * (i + 1));
-
-  for (j = 0; start < end; start++, j++)
-     words[index][j] = str[start];
-  words[index][j] = '\0';
+	i = end - start;
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+	for (j = 0; start < end; start++, j++)
+	{
+		words[index][j] = str[start];
+		words[index][j] = '\0';
+	}
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,14 +18,11 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	
 	array = (int **)malloc(sizeof(int *) * height);
-	
 	if (array == NULL)
 	{
 		return (NULL);
 	}
-	
 	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
