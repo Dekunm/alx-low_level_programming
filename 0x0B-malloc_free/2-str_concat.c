f30de57c933f776dfed7feef422a1c6314d5809d
Dekunm <kunmzxlee@gmail.com>
2023-03-13 15:21:07 -0700

    Malloc str concst

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j, len1, len2, len;
 	char *result;
-	
+
 	len1 = len2 = 0;
 	if (s1 != NULL)
 	{
@@ -26,7 +26,6 @@ char *str_concat(char *s1, char *s2)
 			len1++;
 		}
 	}
-	
 	if (s2 != NULL)
 	{
 		i = 0;
@@ -35,15 +34,12 @@ char *str_concat(char *s1, char *s2)
 			len2++;
 		}
 	}
-	
 	len = len1 + len2;
 	result = (char *)malloc(sizeof(char) * (len + 1));
-	
 	if (result == NULL)
 	{
 		return (NULL);
 	}
-	
 	for (i = 0; i < len1; i++)
 	{
 		result[i] = s1[i];
