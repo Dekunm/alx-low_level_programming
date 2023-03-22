347ad449485dc585bff4a7550dbce3e7f725130e
Dekunm <kunmzxlee@gmail.com>
2023-03-22 10:33:55 -0700

    These are files  for  function array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,8 @@
 void print_name(char  *name, void (*f)(char *));
 void array_iterator(int *array, size_t  size, void (*action)(int));
 int int_index(int  *array, int size, int (*cmp)(int));
-int  (*get_op_func(char *s))(int, int);i
+int  (*get_op_func(char *s))(int, int);
+
 typedef enum bool
 {
 	TRUE = 1,
